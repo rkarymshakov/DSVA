25e664e48f43085d781ac19f90c3215d30d44bd1
Rakhman <karymshakovv@gmail.com>
2025-12-30 00:17:33 +0100

    fix sh.

--- start_nodes.sh
+++ start_nodes.sh
@@ -32,8 +32,6 @@ fi
 # Locate the Fat JAR
 FAT_JAR_PATH=$(ls $(pwd)/target/*-jar-with-dependencies.jar | head -n 1)
 echo "JAR created: $FAT_JAR_PATH"
-FAT_JAR_PATH=$(pwd)/$FAT_JAR_NAME
-echo "JAR created: $FAT_JAR_PATH"
 
 ### 3. DISTRIBUTE & START (REMOTE VMs)
 # We use sshpass to handle passwords automatically
