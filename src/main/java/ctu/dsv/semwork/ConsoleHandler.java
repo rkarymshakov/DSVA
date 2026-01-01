3d16ceb90d397c69ad47b066b1a6fdc0ac28a05c
Rakhman <karymshakovv@gmail.com>
2026-01-01 22:29:35 +0100

    Refactor code.

--- src/main/java/ctu/dsv/semwork/ConsoleHandler.java
+++ src/main/java/ctu/dsv/semwork/ConsoleHandler.java
@@ -14,7 +14,7 @@ public class ConsoleHandler implements Runnable {
     private final BufferedReader reader;
     private final PrintStream out = System.out;
     private final PrintStream err = System.err;
-    private Node currentNode;
+    private NodeImpl currentNode;
 
     public ConsoleHandler(NodeImpl myNode) {
         this.currentNode = myNode;
@@ -33,19 +33,20 @@ public class ConsoleHandler implements Runnable {
 
         try {
             switch (command) {
-                case "connect":
-                    if (parts.length < 3) {
-                        out.println("Usage: connect <hostname> <port>");
-                        break;
-                    }
-                    connect(parts[1], Integer.parseInt(parts[2]));
-                    break;
+//                case "connect":
+//                    if (parts.length < 3) {
+//                        out.println("Usage: connect <hostname> <port>");
+//                        break;
+//                    }
+//                    connect(parts[1], Integer.parseInt(parts[2]));
+//                    break;
                 case "addnode":
                     if (parts.length < 3) {
                         out.println("Usage: addnode <hostname> <port>");
                         break;
                     }
-                    addNode(parts[1], Integer.parseInt(parts[2]));
+                    currentNode.joinNetwork((parts[1]), Integer.parseInt(parts[2]));
+//                    addNode(parts[1], Integer.parseInt(parts[2]));
                     break;
                 case "leave":
                     currentNode.leave();
@@ -107,16 +108,16 @@ public class ConsoleHandler implements Runnable {
         }
     }
 
-    private void connect(String hostname, int port) {
-        try {
-            Registry registry = LocateRegistry.getRegistry(hostname, port);
-            currentNode = (Node) registry.lookup(String.valueOf(port));
-            out.println("Connected to node ID: " + currentNode.getNodeId());
-        } catch (Exception e) {
-            err.println("Connection failed: " + e.getMessage());
-            currentNode = null;
-        }
-    }
+//    private void connect(String hostname, int port) {
+//        try {
+//            Registry registry = LocateRegistry.getRegistry(hostname, port);
+//            currentNode = (Node) registry.lookup(String.valueOf(port));
+//            out.println("Connected to node ID: " + currentNode.getNodeId());
+//        } catch (Exception e) {
+//            err.println("Connection failed: " + e.getMessage());
+//            currentNode = null;
+//        }
+//    }
 
     private void addNode(String hostname, int port) {
         try {
