461526635ba2fef339e8b0ceb577c253cb3e9beb
Rakhman <karymshakovv@gmail.com>
2026-01-10 21:43:21 +0100

    refactor code

--- delay_control_nodes.sh
+++ delay_control_nodes.sh
@@ -4,22 +4,6 @@ source bash_variables.sh
 SLEEP_TIME=2
 echo "   TEST SCENARIO (Using Remote IPs, 5 Nodes)"
 
-echo "[STEP] Node 2 (${NODE_IP[2]}) joining Node 1 (${NODE_IP[1]})..."
-curl -X POST http://${NODE_IP[2]}:${NODE_API_PORT[2]}/join/${NODE_IP[1]}/${NODE_PORT[1]}
-sleep ${SLEEP_TIME}
-
-echo "[STEP] Node 3 (${NODE_IP[3]}) joining Node 1 (${NODE_IP[1]})..."
-curl -X POST http://${NODE_IP[3]}:${NODE_API_PORT[3]}/join/${NODE_IP[1]}/${NODE_PORT[1]}
-sleep ${SLEEP_TIME}
-
-echo "[STEP] Node 4 (${NODE_IP[4]}) joining Node 1 (${NODE_IP[1]})..."
-curl -X POST http://${NODE_IP[4]}:${NODE_API_PORT[4]}/join/${NODE_IP[1]}/${NODE_PORT[1]}
-sleep ${SLEEP_TIME}
-
-echo "[STEP] Node 5 (${NODE_IP[5]}) joining Node 1 (${NODE_IP[1]})..."
-curl -X POST http://${NODE_IP[5]}:${NODE_API_PORT[5]}/join/${NODE_IP[1]}/${NODE_PORT[1]}
-sleep ${SLEEP_TIME}
-
 echo -e "\n[STEP] Status Check:"
 curl http://${NODE_IP[1]}:${NODE_API_PORT[1]}/status
 echo ""
@@ -34,8 +18,8 @@ curl -s -X POST http://${NODE_IP[2]}:${NODE_API_PORT[2]}/enter-cs &
 
 sleep ${SLEEP_TIME}
 
-echo -e "\n[STEP] Node 1 writing variable 11..."
-curl -X POST http://${NODE_IP[1]}:${NODE_API_PORT[1]}/var/11
+echo -e "\n[STEP] Node 1 writing variable 33..."
+curl -X POST http://${NODE_IP[1]}:${NODE_API_PORT[1]}/var/33
 curl -X POST http://${NODE_IP[1]}:${NODE_API_PORT[1]}/leave-cs
 
 sleep ${SLEEP_TIME}
@@ -57,8 +41,8 @@ echo "Node 5: "
 curl http://${NODE_IP[5]}:${NODE_API_PORT[5]}/var
 echo ""
 
-echo -e "\n[STEP] Node 2 writing variable 22..."
-curl -X POST http://${NODE_IP[2]}:${NODE_API_PORT[2]}/var/22
+echo -e "\n[STEP] Node 2 writing variable 44..."
+curl -X POST http://${NODE_IP[2]}:${NODE_API_PORT[2]}/var/44
 curl -X POST http://${NODE_IP[2]}:${NODE_API_PORT[2]}/leave-cs
 
 sleep ${SLEEP_TIME}
