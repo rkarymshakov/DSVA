c9923d3876a595a4b751000dc84a8058dbefc413
Rakhman <karymshakovv@gmail.com>
2026-01-11 00:38:57 +0100

    refactor code

---
+++ 2control_nodes.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+source bash_variables.sh
+
+SLEEP_TIME=2
+echo "   TEST SCENARIO (Using Remote IPs, 5 Nodes)"
+
+echo -e "\n[STEP] Status Check:"
+curl http://${NODE_IP[5]}:${NODE_API_PORT[5]}/status
+echo ""
+
+echo -e "\n[STEP] Requesting CS on all nodes..."
+curl -s -X POST http://${NODE_IP[1]}:${NODE_API_PORT[1]}/enter-cs &
+curl -s -X POST http://${NODE_IP[2]}:${NODE_API_PORT[2]}/enter-cs &
+
+sleep ${SLEEP_TIME}
+
+echo -e "\n[STEP] Node 1 writing variable 11..."
+curl -X POST http://${NODE_IP[1]}:${NODE_API_PORT[1]}/var/11
+curl -X POST http://${NODE_IP[1]}:${NODE_API_PORT[1]}/leave-cs
+
+sleep ${SLEEP_TIME}
+
+echo -e "\n[STEP] Reading shared variable from 5. nodes:"
+curl http://${NODE_IP[5]}:${NODE_API_PORT[5]}/var
+echo ""
+
+echo -e "\n[STEP] Node 2 writing variable 22..."
+curl -X POST http://${NODE_IP[2]}:${NODE_API_PORT[2]}/var/22
+curl -X POST http://${NODE_IP[2]}:${NODE_API_PORT[2]}/leave-cs
+
+sleep ${SLEEP_TIME}
+
+echo -e "\n[STEP] Reading shared variable from 5. nodes:"
+curl http://${NODE_IP[5]}:${NODE_API_PORT[5]}/var
+echo ""
+
+echo "Test Complete."
