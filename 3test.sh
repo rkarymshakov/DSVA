3ecd28ab7f00f05d4c357a8a7635221276591f18
Rakhman <karymshakovv@gmail.com>
2026-01-11 22:39:14 +0100

    refactor code

--- 3test.sh
+++ 3test.sh
@@ -5,7 +5,7 @@ SLEEP_TIME=1
 echo "   Test without topology, with delay (Using Remote IPs, 5 Nodes)"
 
 echo "[STEP] Setting 2s message delay on Node 1..."
-curl -X POST http://${NODE_IP[1]}:${NODE_API_PORT[1]}/delay/5000
+curl -X POST http://${NODE_IP[1]}:${NODE_API_PORT[1]}/delay/1000
 sleep ${SLEEP_TIME}
 
 echo -e "\n[STEP] Status Check:"
