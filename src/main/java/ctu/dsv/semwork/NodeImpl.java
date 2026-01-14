bfb7506f4e2547243f6e5d27d088171fa5b9ea04
Rakhman <karymshakovv@gmail.com>
2026-01-14 13:46:32 +0100

    refactor code

--- src/main/java/ctu/dsv/semwork/NodeImpl.java
+++ src/main/java/ctu/dsv/semwork/NodeImpl.java
@@ -96,6 +96,7 @@ public class NodeImpl extends UnicastRemoteObject implements Node {
     public void joinNetwork(String ip, int port) throws RemoteException {
         inCriticalSection = false;
         wantCS = false;
+        requestQueue.clear();
         repliesReceivedForMyRequest.clear();
         try {
             Registry registry = LocateRegistry.getRegistry(ip, port);
